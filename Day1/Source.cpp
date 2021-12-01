4a62d5d636c5b8a393dab2e08051359dd4a7f709
wisemankaitlyn <68436733+wisemankaitlyn@users.noreply.github.com>
2021-12-01 06:00:25 -0500

    Day 1

--- Day1/Source.cpp
+++ Day1/Source.cpp
@@ -2,7 +2,57 @@
 // Advent of Code 2021
 // Kaitlyn Wiseman
 
+#include<iostream>
+#include<vector>
+#include<fstream>
+
+using namespace std;
+
 void main()
 {
+	// part 1
+
+	vector<int> depths;
+	ifstream in;
+	in.open("input.txt");
+
+	int lastTemp, temp, numInc = 0;
+
+	in >> lastTemp;
+	while (!in.eof())
+	{
+		in >> temp;
+		if (temp > lastTemp) numInc++;
+
+		lastTemp = temp;
+	}
+	in.close();
+
+	cout << "Part 1: the number of increases is " << numInc << endl;
+
+	// part 2
+
+	int sum, lastSum = 0;
+	temp, numInc = 0;
+
+	in.open("input.txt");
+	while (!in.eof())
+	{
+		in >> temp;
+		depths.push_back(temp);
+	}
+	in.close();
+
+	lastSum = depths.at(0) + depths.at(1) + depths.at(2);
+	for (int i = 1; i + 2 < depths.size(); i++)
+	{
+		sum = depths.at(i) + depths.at(i + 1) + depths.at(i + 2);
+
+		if (sum > lastSum) numInc++;
+		lastSum = sum;
+	}
+
+	cout << "Part 2: the number of increases is " << numInc << endl;
+
 	return;
 }
